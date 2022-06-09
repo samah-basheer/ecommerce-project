fb46384ad0ba94c2c8ee3ce9e1d09103f61ce129
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-09 16:58:10 +0300

    added the routes for category

--- laravel-server/app/Http/Controllers/CategoryController.php
+++ laravel-server/app/Http/Controllers/CategoryController.php
@@ -3,83 +3,48 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function index() {
+        $categories = Category::all();
+        return response()->json([
+            "status" => "Success",
+            "categories" => $categories
+        ], 200);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function create(Request $request) {
+        $category = new Category;
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->save();
+        return response()->json([
+            "status" => "Success"
+        ], 200);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function edit($id) {
+        $category = Category::findOrFail($id);
+        return response()->json([
+            "status" => "Success",
+            "category" => $category
+        ]);
     }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
+    public function update(Request $request, $id) {
+        $category = Category::findOrFail($id);
+        $category->name = $request->name;
+        $category->description = $request->description;
+        $category->update();
+        return response()->json([
+            "status" => "Success"
+        ]);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Category $category)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Category $category)
-    {
-        //
+    public function destroy($id) {
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return response()->json([
+            "status" => "Success"
+        ], 200);
     }
 }
--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,11 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function () {
+//  product apis
     Route::prefix('product')->group(function () {
         Route::get('/', [ProductController::class, 'index']);
         Route::post('/create', [ProductController::class, 'create']);
         Route::get('/edit/{id}', [ProductController::class, 'edit']);
         Route::post('/update/{id}', [ProductController::class, 'update']);
         Route::delete('/delete/{id}', [ProductController::class, 'destroy']);
+    });
+
+//  category apis
+    Route::prefix('category')->group(function () {
+        Route::get('/', [CategoryController::class, 'index']);
+        Route::post('/create', [CategoryController::class, 'create']);
+        Route::get('/edit/{id}', [CategoryController::class, 'edit']);
+        Route::post('/update/{id}', [CategoryController::class, 'update']);
+        Route::delete('/delete/{id}', [CategoryController::class, 'destroy']);
     });
 });
