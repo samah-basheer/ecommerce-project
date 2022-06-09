ba8a790713bd960f56ba75de04c55328db212993
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-09 23:42:18 +0300

    added responsive menu with styles and js

--- frontend-user/includes/header.php
+++ frontend-user/includes/header.php
@@ -5,6 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../assets/css/styles.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/82f797382f.js" crossorigin="anonymous"></script>
     <title>E-commerce Site</title>
 </head>
 <body>
+    <nav class="navbar" >
+        <div class="logo_header">
+            <a href="/" class="logo">E-commerce Site</a>
+            <button class="navbar_toggle">
+                <i class="fas fa-bars"></i>
+            </button>
+        </div>
+        <ul class="main_nav">
+            <li>
+                <a href="#" class="nav_links">Home</a>
+            </li>
+            <li>
+                <a href="#" class="nav_links">Products</a>
+            </li>
+            <li>
+                <a href="#" class="nav_links">About</a>
+            </li>
+            <li>
+                <a href="#" class="nav_links">Contact</a>
+            </li>
+            <li>
+                <a href="#" class="nav_links">Blog</a>
+            </li>
+        </ul>
+    </nav>
