b0f44948fb185283624e95b3849f4106cfb90cfb
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-09 23:17:59 +0300

    created the includes and pages folder

--- frontend-user/index.php
+++ frontend-user/index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="../assets/css/styles.css" rel="stylesheet">
-    <title>E-commerce Site</title>
-</head>
-<body>
+<?php include_once './includes/header.php' ?>
 
-</body>
-<script src="../assets/js/main.js"></script>
-</html>
+<?php include_once './includes/footer.php' ?>
