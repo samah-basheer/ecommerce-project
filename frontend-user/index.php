145931a513f8654311db8d712e29b845eba9557a
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-10 12:23:16 +0300

    added products loop

--- frontend-user/index.php
+++ frontend-user/index.php
@@ -2,14 +2,64 @@
 <div class="banner">
     <img src="./assets/img/banner-serum.jpg">
 </div>
-<div class="about wrapper">
+<div class="about wrapper wrapper-content">
     <div>
         <img src="./assets/img/beauty.png">
     </div>
     <div>
-        <h2 class="title">About us</h2>
+        <h2 class="title">ABOUT US</h2>
         <div class="spacer"></div>
         <p class="about-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
+</div>
+<div class="latest-product wrapper">
+    <h2 class="title text-center">LATEST PRODUCTS</h2>
+    <div class="spacer"></div>
+    <div class="products wrapper-content">
+        <div class="single-product">
+            <div class="product-img">
+                <img src="./assets/img/MICELLAR.png">
+            </div>
+            <div class="product-title">Micellar Water</div>
+            <div class="product-price">LBP 39,000</div>
+        </div>
+        <div class="single-product">
+            <div class="product-img">
+                <img src="./assets/img/522-prom-night.jpg">
+            </div>
+            <div class="product-title">Micellar Water</div>
+            <div class="product-price">LBP 39,000</div>
+        </div>
+        <div class="single-product">
+            <div class="product-img">
+                <img src="./assets/img/LIPSTICK-TOFFEE.jpg">
+            </div>
+            <div class="product-title">Micellar Water</div>
+            <div class="product-price">LBP 39,000</div>
+        </div>
+        <div class="single-product">
+            <div class="product-img">
+                <img src="./assets/img/WATERPROOF-LIPLINER.jpg">
+            </div>
+            <div class="product-title">Micellar Water</div>
+            <div class="product-price">LBP 39,000</div>
+        </div>
+        <div class="single-product">
+            <div class="product-img">
+                <img src="./assets/img/MICELLAR.png">
+            </div>
+            <div class="product-title">Micellar Water</div>
+            <div class="product-price">LBP 39,000</div>
+        </div>
+        <div class="single-product">
+            <div class="product-img">
+                <img src="./assets/img/MICELLAR.png">
+            </div>
+            <div class="product-title">Micellar Water</div>
+            <div class="product-price">LBP 39,000</div>
+        </div>
+    </div>
+</div>
+<div class="wrapper justify-center">
 </div>
 <?php include_once './includes/footer.php' ?>
