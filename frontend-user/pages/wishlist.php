7c6c7dd2a61300fc8f84d3df91395c08758e9e46
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-10 13:39:01 +0300

    created the wishlist page

--- frontend-user/pages/wishlist.php
+++ frontend-user/pages/wishlist.php
@@ -1,3 +1,8 @@
 <?php include_once '../includes/header.php' ?>
-
+    <div class="page-title">
+        <h1>Wishlist</h1>
+    </div>
+    <div class="wrapper min-height">
+        <h2 class="title">My Wishlist</h2>
+    </div>
 <?php include_once '../includes/footer.php' ?>
