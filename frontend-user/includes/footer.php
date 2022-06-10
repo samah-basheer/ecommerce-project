557286e4eb9e3f4b46c6ee7fd92fd386c10d9a12
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-10 11:58:14 +0300

    added footer section

--- frontend-user/includes/footer.php
+++ frontend-user/includes/footer.php
@@ -1,3 +1,8 @@
+<footer>
+    <div class="footer">
+        <p>Copy Right ©</p>
+    </div>
+</footer>
 </body>
 <script src="../assets/js/main.js"></script>
 </html>
--- frontend-user/index.php
+++ frontend-user/index.php
@@ -2,4 +2,14 @@
 <div class="banner">
     <img src="./assets/img/banner-serum.jpg">
 </div>
+<div class="about wrapper">
+    <div>
+        <img src="./assets/img/beauty.png">
+    </div>
+    <div>
+        <h2 class="title">About us</h2>
+        <div class="spacer"></div>
+        <p class="about-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
 <?php include_once './includes/footer.php' ?>
