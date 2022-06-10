b9d9d773951a5c2d4a8acd58b14d32a1ed629450
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-10 14:43:12 +0300

    added wishlist styles

--- frontend-user/pages/wishlist.php
+++ frontend-user/pages/wishlist.php
@@ -3,6 +3,26 @@
         <h1>Wishlist</h1>
     </div>
     <div class="wrapper min-height">
-        <h2 class="title">My Wishlist</h2>
+        <h2 class="title padding">My Wishlist</h2>
+        <div class="wrapper-content wrapper-wishlist">
+            <div class="left">
+                <i class="fa fa-close"></i>
+                <img src="../assets/img/MICELLAR.png">
+            </div>
+            <div class="right">
+                <h2>Micellar Water</h2>
+                <p>LBP 40,000</p>
+            </div>
+        </div>
+        <div class="wrapper-content wrapper-wishlist">
+            <div class="left">
+                <i class="fa fa-close"></i>
+                <img src="../assets/img/MICELLAR.png">
+            </div>
+            <div class="right">
+                <h2>Micellar Water</h2>
+                <p>LBP 40,000</p>
+            </div>
+        </div>
     </div>
 <?php include_once '../includes/footer.php' ?>
