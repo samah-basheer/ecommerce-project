b3e664dcf6a8ca5d74a9ca49a056ffafe029243c
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-11 23:16:23 +0300

    added styles for close icon

--- frontend-user/pages/wishlist.php
+++ frontend-user/pages/wishlist.php
@@ -15,15 +15,5 @@
 <!--                <p>LBP 40,000</p>-->
 <!--            </div>-->
 <!--        </div>-->
-<!--        <div class="wrapper-content wrapper-wishlist">-->
-<!--            <div class="left">-->
-<!--                <i class="fa fa-close"></i>-->
-<!--                <img src="../assets/img/MICELLAR.png">-->
-<!--            </div>-->
-<!--            <div class="right">-->
-<!--                <h2>Micellar Water</h2>-->
-<!--                <p>LBP 40,000</p>-->
-<!--            </div>-->
-<!--        </div>-->
     </div>
 <?php include_once '../includes/footer.php' ?>
